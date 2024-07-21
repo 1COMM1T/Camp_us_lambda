177224a00b654614ccff5e717b384e4034a7874d
bucky1005 <null00007@naver.com>
2024-07-22 07:03:50 +0900

    feat: CampingFacilitiesEntity 필드 생성
    - 필드 생성
    - 인터페이스 클래스인 LambdaApiCallService.java에서 .java 제거
    
    Fixes: #11

--- lamdba-api-call/src/main/java/com/commit/lamdbaapicall/entity/CampingFacilitiesEntity.java
+++ lamdba-api-call/src/main/java/com/commit/lamdbaapicall/entity/CampingFacilitiesEntity.java
@@ -1,16 +1,39 @@
 package com.commit.lamdbaapicall.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "camping_facilities")
 public class CampingFacilitiesEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long campFacsId;
+    private Long campId;
+    private int facsTypeId;
+    private String internalFacilitiesList;
+    private int toiletCnt;
+    private int showerRoomCnt;
+    private int sinkCnt;
+    private String brazierClass;
+    private String supportFacilities;
+    private String outdoorActivities;
+    private String petAccess;
+    private String firstImageUrl;
+    private String operationDay;
+    private String personalTrailerStatus;
+    private String personalCaravanStatus;
+    private String rentalGearList;
     private String facilityName;
 
 }
