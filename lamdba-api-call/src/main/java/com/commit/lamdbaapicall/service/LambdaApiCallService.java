370a78274c0ac07656342fbc76719a704a442c8e
bucky1005 <null00007@naver.com>
2024-07-21 23:50:59 +0900

    chore: 미사용 의존성 제거
    - 사용하지 않는 의존성 주석 제거

--- lamdba-api-call/src/main/java/com/commit/lamdbaapicall/service/LambdaApiCallService.java
+++ lamdba-api-call/src/main/java/com/commit/lamdbaapicall/service/LambdaApiCallService.java
@@ -1,41 +1,6 @@
 package com.commit.lamdbaapicall.service;
 
+public interface LambdaAPICallService {
 
-import org.springframework.stereotype.Service;
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
-import software.amazon.awssdk.core.SdkBytes;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.lambda.LambdaClient;
-import software.amazon.awssdk.services.lambda.model.InvokeRequest;
-import software.amazon.awssdk.services.lambda.model.InvokeResponse;
-
-import java.nio.charset.StandardCharsets;
-
-@Service
-public class LambdaApiCallService {
-
-    private final LambdaClient lambdaClient;
-
-    public LambdaApiCallService() {
-        this.lambdaClient = LambdaClient.builder()
-                .region(Region.AP_NORTHEAST_2)
-                .credentialsProvider(ProfileCredentialsProvider.create())
-                .build();
-    }
-
-    public LambdaApiCallService(LambdaClient lambdaClient) {
-        this.lambdaClient = lambdaClient;
-    }
-
-    public void invokeLambda(String functionName, String payload) {
-        InvokeRequest invokeRequest = InvokeRequest.builder()
-                .functionName(functionName)
-                .payload(SdkBytes.fromByteArray(payload.getBytes(StandardCharsets.UTF_8)))
-                .build();
-
-        InvokeResponse invokeResponse = lambdaClient.invoke(invokeRequest);
-        String responseString = new String(invokeResponse.payload().asByteArray(), StandardCharsets.UTF_8);
-
-        System.out.println("Lambda Response: " + responseString);
-    }
+//    public void invokeLambda(String functionName, String payload);
 }
