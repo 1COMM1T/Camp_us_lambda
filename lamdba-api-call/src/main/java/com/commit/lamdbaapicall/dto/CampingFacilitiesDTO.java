ef6a3d636e45ebb39685a1c3032501d779655b7b
bucky1005 <null00007@naver.com>
2024-07-23 06:58:38 +0900

    refactor: CampingFacilities 테이블 수정
    - facsName 컬럼 제거
    
    Fixes: #11

--- lamdba-api-call/src/main/java/com/commit/lamdbaapicall/dto/CampingFacilitiesDTO.java
+++ lamdba-api-call/src/main/java/com/commit/lamdbaapicall/dto/CampingFacilitiesDTO.java
@@ -1,45 +1,54 @@
 package com.commit.lamdbaapicall.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
 @Builder
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CampingFacilitiesDTO {
     private Long campFacsId;
     private Long campId;
     private int facsTypeId;
+
     @JsonProperty("sbrsCl")
     private String internalFacilitiesList; // 부대시설
+
     @JsonProperty("toiletCo")
     private int toiletCnt;
+
     @JsonProperty("swrmCo")
     private int showerRoomCnt;
+
     @JsonProperty("btrpCO")
     private int sinkCnt;
+
     @JsonProperty("braizerCl")
     private String brazierClass;
+
     @JsonProperty("featureNm")
     private String supportFacilities;
+
     @JsonProperty("posblFcltyCl")
     private String outdoorActivities;
+
     @JsonProperty("animalCmgCl")
     private String petAccess;
+
     @JsonProperty("firstImageUrl")
     private String firstImageUrl;
+
     @JsonProperty("operDeCl")
     private String operationDay;
+
     @JsonProperty("trlerAcmpnyAt")
     private String personalTrailerStatus;
+
     @JsonProperty("caravAcmpnyAt")
     private String personalCaravanStatus;
+
     @JsonProperty("eqpmnLendCl")
     private String rentalGearList;
-    @JsonProperty("facltNm")
-    private String facilityName;
 }
--- lamdba-api-call/src/main/java/com/commit/lamdbaapicall/entity/CampingFacilitiesEntity.java
+++ lamdba-api-call/src/main/java/com/commit/lamdbaapicall/entity/CampingFacilitiesEntity.java
@@ -19,21 +19,49 @@ public class CampingFacilitiesEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long campFacsId;
-    private Long campId;
-    private int facsTypeId;
-    private String internalFacilitiesList;
-    private int toiletCnt;
-    private int showerRoomCnt;
-    private int sinkCnt;
-    private String brazierClass;
-    private String supportFacilities;
-    private String outdoorActivities;
-    private String petAccess;
-    private String firstImageUrl;
-    private String operationDay;
-    private String personalTrailerStatus;
-    private String personalCaravanStatus;
-    private String rentalGearList;
-    private String facilityName;
 
+    @Column(name = "camp_id")
+    private Long campId;
+
+    @Column(name = "facs_type_id")
+    private int facsTypeId;
+
+    @Column(name = "internal_facilities_list")
+    private String internalFacilitiesList;
+
+    @Column(name = "toilet_cnt")
+    private int toiletCnt;
+
+    @Column(name = "shower_room_cnt")
+    private int showerRoomCnt;
+
+    @Column(name = "sink_cnt")
+    private int sinkCnt;
+
+    @Column(name = "brazier_class")
+    private String brazierClass;
+
+    @Column(name = "support_facilities")
+    private String supportFacilities;
+
+    @Column(name = "outdoor_activities")
+    private String outdoorActivities;
+
+    @Column(name = "pet_access")
+    private String petAccess;
+
+    @Column(name = "first_image_url")
+    private String firstImageUrl;
+
+    @Column(name = "operation_day")
+    private String operationDay;
+
+    @Column(name = "personal_trailer_status")
+    private String personalTrailerStatus;
+
+    @Column(name = "personal_caravan_status")
+    private String personalCaravanStatus;
+
+    @Column(name = "rental_gear_list")
+    private String rentalGearList;
 }
